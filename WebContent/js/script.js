b19e33912b0c5010ec90290c5f9547c172e95f93
Basannavar, Namita <namita4580@gmail.com>
2019-12-09 14:06:01 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,31 @@
-// Include form validation functions here
+
+function validation() {
+                var a = document.forms["form"]["txttitle"].value;
+                if(a== "") {
+                                alert("Title is Required");
+                                return false;
+                           }
+                if ((a.length < 2) || (a.length > 65)) {
+                                alert("Title should have 2 to 65 characters.");
+                                return false;
+                            }
+                var b = document.forms["form"]["txtGross"].value;
+                if(b== "") {
+                                alert("Gross is required.");
+                                return false;
+                           }
+                if(/[^0-9/]/.test(b)) {
+                                alert("Gross has to be a number.");
+                                return false;
+                           }
+                var c = document.forms["form"]["txtDoL"].value;
+                if(c== "") {
+                                alert("Date of launch is required.");
+                                return false;
+                           }
+                var d = document.forms["form"]["Genre"].value;
+                if(d== "") {
+                                alert("Genre is required.");
+                                return false;
+                           }
+}
